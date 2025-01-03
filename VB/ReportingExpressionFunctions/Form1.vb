f249fc7d3392715812a520a50b422ffc5ef886f9
DevExpressExampleBot <fakeEmail>
2025-01-03 13:15:54 +0400

    autogenerate VB

---
+++ VB/ReportingExpressionFunctions/Form1.vb
@@ -0,0 +1,10 @@
+Namespace ReportingExpressionFunctions
+
+    Public Partial Class Form1
+        Inherits Form
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
+End Namespace
---
+++ VB/ReportingExpressionFunctions/Report1.vb
@@ -0,0 +1,14 @@
+Imports DevExpress.XtraReports.UI
+Imports System.ComponentModel
+Imports System.Drawing
+
+Namespace ReportingExpressionFunctions
+
+    Public Partial Class Report1
+        Inherits XtraReport
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
+End Namespace
